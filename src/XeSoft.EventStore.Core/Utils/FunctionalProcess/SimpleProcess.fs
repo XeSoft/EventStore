73c2060379ea1a7d64a7b180b0736da75eddc59a
Kasey Speakman <kasey@cornerspeed.com>
2025-02-01 16:09:28 -0600

    rename in prep for full process with services

--- src/XeSoft.EventStore.Core/Utils/FunctionalProcess/SimpleProcess.fs
+++ src/XeSoft.EventStore.Core/Utils/FunctionalProcess/SimpleProcess.fs
@@ -3,34 +3,34 @@
 open System.Threading.Tasks
 open FSharp.Control
 
-type Process<'initArg, 'model, 'msg, 'effect> = {
+type SimpleProcess<'initArg, 'model, 'msg, 'effect> = {
     Init: 'initArg -> 'model * 'msg
     Update: 'model -> 'msg -> 'model * List<'effect>
     //Services: 'model -> List<'service>
     //Stopped: 'model -> bool
 }
 
-type SideEffects<'effect, 'msg> = {
+type SimpleSideEffects<'effect, 'msg> = {
     Perform: 'effect -> Task<'msg>
     //StartService: 'service -> TaskSeq<'msg> * IDisposable
     //Stop: unit -> unit
 }
 
 
-module Process =
+module SimpleProcess =
 
-    let test (proc: Process<'initArg, 'model, 'msg, 'effect>) (model: 'model) (msgs: List<'msg>) =
+    let test (proc: SimpleProcess<'initArg, 'model, 'msg, 'effect>) (model: 'model) (msgs: List<'msg>) =
         let init = model, []
         let update (model, effects) msg =
             let (model, newEffects) = proc.Update model msg
             (model, List.append effects newEffects)
         List.fold update init msgs
 
-    let run (sideEffects: SideEffects<'effect, 'msg>) (proc: Process<'initArg, 'model, 'msg, 'effect>) (initArg: 'initArg) : Task<Result<'model, exn>> =
+    let run (sideEffects: SimpleSideEffects<'effect, 'msg>) (proc: SimpleProcess<'initArg, 'model, 'msg, 'effect>) (initArg: 'initArg) : Task<Result<'model, exn>> =
         task {
             try
                 let initModel, initMsg = proc.Init initArg
-                // while b/c recursive Task doesn't optimize to while loop, and can result in OOM exception
+                // while, b/c recursive Task doesn't do tail call optimization, and can result in OOM exception
                 let mutable model = initModel
                 let mutable msgs = [initMsg]
                 while not (List.isEmpty msgs) do
