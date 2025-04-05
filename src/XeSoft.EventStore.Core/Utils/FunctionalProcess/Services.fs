25b81af4e96bc944cfcfe594bcb6827d6204aea9
kspeakman <>
2025-04-05 10:02:19 -0500

    rename for readability

--- src/XeSoft.EventStore.Core/Utils/FunctionalProcess/Services.fs
+++ src/XeSoft.EventStore.Core/Utils/FunctionalProcess/Services.fs
@@ -12,21 +12,20 @@ module Services =
     /// toStop - services which were active but are missing in the new service list, 
     /// toKeep - services which were active and still in the new service list, 
     /// toStart - services which were not active but are in the new service list
-    let diff (activeServices: List<ServiceId * CancellationTokenSource>) (services: List<ServiceId * 'service>) =
-        let stopped, running = activeServices |> List.partition (fun (_svcId, cancelSource) -> cancelSource.IsCancellationRequested)
-        let keys = running |> List.map fst |> Set.ofList
-        let (_dupes, _newKeys, _newSvcs) as init =
-            List.empty, Set.empty, List.empty
-        let update (svcId, svc) (dupes, newKeys, newSvcs) =
-            if Set.contains svcId newKeys then
-                svcId :: dupes, newKeys, newSvcs
-            else
-                dupes, Set.add svcId newKeys, (svcId, svc) :: newSvcs
-        let dupes, newKeys, newSvcs =
-            List.foldBack update services init
-        if keys = newKeys then
+    let diff (activeServices: List<ServiceId * CancellationTokenSource>) (requestedServices: List<ServiceId * 'service>) =
+        let stopped, running = activeServices |> List.partition (fun (_, cts) -> cts.IsCancellationRequested)
+        let oldServiceIds = running |> List.map fst |> Set.ofList
+        let dupes, serviceIds, services =
+            let update (serviceId, service) (dupes, serviceIds, services) =
+                if Set.contains serviceId serviceIds then
+                    serviceId :: dupes, serviceIds, services
+                else
+                    dupes, Set.add serviceId serviceIds, (serviceId, service) :: services
+            (List.empty, Set.empty, List.empty)
+            |> List.foldBack update requestedServices 
+        if serviceIds = oldServiceIds then
             stopped, dupes, [], running, []
         else
-            let toKeep, toStop = running |> List.partition (fun (k, _) -> Set.contains k newKeys)
-            let toStart = newSvcs |> List.filter (fun (k, _) -> not (Set.contains k keys))
+            let toKeep, toStop = running |> List.partition (fun (k, _) -> Set.contains k serviceIds)
+            let toStart = services |> List.filter (fun (k, _) -> not (Set.contains k oldServiceIds))
             stopped, dupes, toStop, toKeep, toStart
